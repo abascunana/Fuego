32a9bf91be93b939437098362b83e84cfc4434db
abascunana <abascunanasanchez@cifpfbmoll.eu>
2023-02-01 20:34:26 +0100

    Fuego convincente

--- src/FuegoModel.java
+++ src/FuegoModel.java
@@ -1,75 +1,56 @@
 import java.awt.*;
-
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class FuegoModel extends Canvas implements Runnable{
-    private int[][] data ;
+public class FuegoModel extends Canvas implements Runnable {
+    private int[][] data;
     private Graphics2D canvasGraphics;
 
     public void Pintar() {
-
-        this.setBackground(Color.white);
+        this.setBackground(Color.gray);
         canvasGraphics = (Graphics2D) this.getGraphics();
         Random r = new Random();
-        System.out.println(this.getWidth());
-        //Puntos de llama inicial
-        ArrayList<Color> colors = new ArrayList<>();
-        //paleta de colores, hace falta modificar para realismo
-        for (int i = 0; i <256 ; i++) {
 
-            Color color = new Color(255,255-i,255-i,255);
+        // Paleta de colores
+        ArrayList<Color> colors = new ArrayList<>();
+        for (int i = 0; i < 256; i++) {
+            int alpha = i;
+            Color color = new Color(255, 0, 0, alpha);
             colors.add(color);
         }
-        //Con this
+
+        // Creación de la matriz de temperatura
         data = new int[1920][1080];
 
-        for (int i = 1; i < getWidth()-1; i++) {
-            for (int j = 1; j < 254; j++) {
-                //Valor espurnas
+        // Inicialización de los puntos de llama
+        for (int i = 0; i < getWidth(); i++) {
+            for (int j = getHeight()-5; j < getHeight(); j++) {
                 int porciento = r.nextInt(100);
-                if (porciento < 20){
-
-                    data[i][j] = ((data[i][j] + data[i+1][j] + data[i-1][j] + data[i][j+1])/4);
-                    //Intensidad en la paleta de colores (colors)
-                    canvasGraphics.setColor(colors.get(j));
-                    canvasGraphics.drawRect(i,j, 1,1);
-
-
+                if (porciento < 20) {
+                data[i][j] = 255;
                 }
 
             }
+        }
 
-
+        // Propagación del fuego
+        for (int i = 1; i < getWidth()-1; i++) {
+            for (int j = getHeight()-4; j >= 0; j--) {
+                int porciento = r.nextInt(100);
+                if (porciento < 80) {
+                    data[i][j] = (data[i - 1][j + 1] + data[i][j + 1] + data[i + 1][j + 1] + data[i][j + 1]) / 4;
+                    canvasGraphics.setColor(colors.get(data[i][j]));
+                    canvasGraphics.drawRect(i, j, 1, 1);
+                }
             }
-
-
-
-
-
-
-
+        }
     }
-
 
     @Override
     public void run() {
-    while (true){
-        Pintar();
-        repaint();
-
+        while (true) {
+            Pintar();
+            repaint();
+        }
     }
-
-
-    }
-
-
-
 }
-
-
-
-
--- src/FuegoView.java
+++ src/FuegoView.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class FuegoView extends JFrame implements Runnable{
 //Crear
@@ -8,10 +9,13 @@ public class FuegoView extends JFrame implements Runnable{
     }
     public void CrearMiventana(){
         setTitle("Miventana");
+//        this.setLayout(new GridBagLayout());
         setSize(500,500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         FuegoModel fuego = new FuegoModel();
         this.getContentPane().add(fuego);
+        this.add(fuego);
         this.setVisible(true);
         fuego.run();
 
