3ab6705491fafc8e086130962c9cd731456cfd48
abascunana <abascunanasanchez@cifpfbmoll.eu>
2023-02-15 17:55:35 +0100

    commit

---
+++ src/FuegoController.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+
+public class FuegoController implements Runnable{
+    private Color colorBC;
+    private Color colorFr; //3 colores
+    private FuegoModel model;
+    private boolean deadFire = false;
+
+    public void setDeadFire() {
+        this.deadFire = !this.deadFire;
+    }
+
+
+
+
+    public FuegoModel getModel() {
+        return model;
+    }
+
+    public void setModel(FuegoModel model) {
+        this.model = model;
+    }
+
+    public FuegoController(FuegoModel model) {
+        this.model = model;
+    }
+
+    public void setColorBC(Color colorBC) {
+        this.colorBC = colorBC;
+    }
+
+    public void setColorFr(Color colorFr) {
+        this.colorFr = colorFr;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            if (colorBC!=null){
+                getModel().setColorBC(colorBC);
+            }
+            getModel().setDeadFire(deadFire);
+        }
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        FuegoModel fuegoModel = new FuegoModel();
+        FuegoController fuegoController = new FuegoController(fuegoModel);
+        fuegoModel.setFuegoController(fuegoController);
+        FuegoView fuegoView = new FuegoView(fuegoModel);
+
+
+
+    }
+}
